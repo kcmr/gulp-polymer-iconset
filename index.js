cb57da787eb100939fed9c1b409cf80d261cc243
Kus Cámara <kcmr@users.noreply.github.com>
2016-09-01 09:15:30 +0200

    change indentation

--- index.js
+++ index.js
@@ -13,128 +13,127 @@ var renderIconSetHtml   = _.template(iconSetTemplate);
 
 // default values for options
 var DEFAULT_OPTIONS = {
-    /**
-     * Name to be given to the icon
-     * @param  {Vinyl} file
-     * @return {String}     the id of the icon
-     */
-    iconId: function (file) {
-        return path.basename(file.path, '.svg');
-    },
+  /**
+   * Name to be given to the icon
+   * @param  {Vinyl} file
+   * @return {String}     the id of the icon
+   */
+  iconId: function (file) {
+    return path.basename(file.path, '.svg');
+  },
 
-    /**
-     * Size
-     * @type {Number}
-     */
-    iconSize: 24,
+  /**
+   * Size
+   * @type {Number}
+   */
+  iconSize: 24,
 
-    // path
-    ironIconsetSvgPath: '../iron-iconset-svg/iron-iconset-svg.html',
+  // path
+  ironIconsetSvgPath: '../iron-iconset-svg/iron-iconset-svg.html',
 
-    /**
-     * Iconset file name
-     * @type {String}
-     */
-    fileName: null
+  /**
+   * Iconset file name
+   * @type {String}
+   */
+  fileName: null
 };
 
 function polymerIconset(options) {
 
-    // set default options
-    options = options || {};
-    _.defaults(options, DEFAULT_OPTIONS);
+  // set default options
+  options = options || {};
+  _.defaults(options, DEFAULT_OPTIONS);
 
-    // check for required options
-    if (!options.iconsetName) {
+  // check for required options
+  if (!options.iconsetName) {
+    throw new gulpUtil.PluginError(
+      'gulp-polymer-iconset',
+      'iconsetName option is required'
+      );
+  }
+
+  // default filename if is not set
+  if (!options.fileName) {
+    options.fileName = options.iconsetName + '.html';
+  }
+
+  // start an empty icons string
+  var iconsSvgString = '';
+
+  function bufferContents(file, encoding, cb) {
+
+    // evaluate options according to file
+    var iconId = (typeof options.iconId === 'function') ?
+    options.iconId(file) : options.iconId;
+    var iconSelector = (typeof options.iconSelector === 'function') ?
+    options.iconSelector(file) : options.iconSelector;
+
+    if (file.isNull()) {
+      // return empty file
+      return cb(null, file);
+    }
+
+    if (file.isBuffer()) {
+      // build a cheerio dom
+      var $ = cheerio.load(file.contents.toString(encoding), {
+        xmlMode: true
+      });
+
+      // find the svg node
+      var svgNode = $('svg');
+
+      // check if the icon is made of multiple nodes
+      var svgNodeContents = svgNode.children();
+
+      if (svgNodeContents.length === 1 && $(svgNodeContents[0]).is('g')) {
+          // the icon is ready to be added to the iconset file
+
+          // give id to the iconNode
+          $(svgNodeContents[0]).attr('id', iconId);
+
+          iconsSvgString += $.xml(svgNodeContents[0]);
+        } else {
+          // the icon is not ready to be added,
+          // we must wrap it with an 'g' (group) tag
+          // before adding
+
+          iconsSvgString += '<g id="' + iconId + '">\n' + $.xml(svgNodeContents) + '\n</g>';
+        }
+      }
+
+      if (file.isStream()) {
         throw new gulpUtil.PluginError(
-            'gulp-polymer-iconset',
-            'iconsetName option is required'
+          'gulp-polymer-iconset',
+          'streams not currently supported'
         );
+      }
+
+      // invoke callback and pass no files
+      cb();
     }
 
-    // default filename if is not set
-    if (!options.fileName) {
-        options.fileName = options.iconsetName + '.html';
-    }
+  function endStream(cb) {
+    // set the iconsSvgString onto options
+    var renderOptions = _.assign({
+      iconsSvgString: iconsSvgString,
+    }, options);
 
-    // start an empty icons string
-    var iconsSvgString = '';
+    // render the template
+    var iconSetHtml = renderIconSetHtml(renderOptions);
 
-    function bufferContents(file, encoding, cb) {
+    // create the file object
+    var file = new gulpUtil.File({
+      path: options.fileName,
+      contents: new Buffer(iconSetHtml),
+    });
 
-        // evaluate options according to file
-        var iconId = (typeof options.iconId === 'function') ?
-            options.iconId(file) : options.iconId;
-        var iconSelector = (typeof options.iconSelector === 'function') ?
-            options.iconSelector(file) : options.iconSelector;
+    // send the file downwards the stream
+    this.push(file);
 
-        if (file.isNull()) {
-            // return empty file
-            return cb(null, file);
-        }
+    cb();
+  }
 
-        if (file.isBuffer()) {
-            // build a cheerio dom
-            var $ = cheerio.load(file.contents.toString(encoding), {
-                xmlMode: true
-            });
-
-            // find the svg node
-            var svgNode = $('svg');
-
-            // check if the icon is made of multiple nodes
-            var svgNodeContents = svgNode.children();
-
-            if (svgNodeContents.length === 1 && $(svgNodeContents[0]).is('g')) {
-                // the icon is ready to be added to the iconset file
-
-                // give id to the iconNode
-                $(svgNodeContents[0]).attr('id', iconId);
-
-                iconsSvgString += $.xml(svgNodeContents[0]);
-            } else {
-                // the icon is not ready to be added,
-                // we must wrap it with an 'g' (group) tag
-                // before adding
-
-                iconsSvgString += '<g id="' + iconId + '">\n' + $.xml(svgNodeContents) + '\n</g>';
-            }
-        }
-
-        if (file.isStream()) {
-            throw new gulpUtil.PluginError(
-                'gulp-polymer-iconset',
-                'streams not currently supported'
-            );
-        }
-
-        // invoke callback and pass no files
-        cb();
-    }
-
-    function endStream(cb) {
-
-        // set the iconsSvgString onto options
-        var renderOptions = _.assign({
-            iconsSvgString: iconsSvgString,
-        }, options);
-
-        // render the template
-        var iconSetHtml = renderIconSetHtml(renderOptions);
-
-        // create the file object
-        var file = new gulpUtil.File({
-            path: options.fileName,
-            contents: new Buffer(iconSetHtml),
-        });
-
-        // send the file downwards the stream
-        this.push(file);
-
-        cb();
-    }
-
-    return through2.obj(bufferContents, endStream);
+  return through2.obj(bufferContents, endStream);
 }
 
 module.exports = polymerIconset;
